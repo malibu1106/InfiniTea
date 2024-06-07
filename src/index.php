b5585d481fc3cae9c7c36765b8b29ce4f280f246
Malibu <malibu1106@gmail.com>
2024-06-07 16:40:28 +0200

    Initialization

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="JS/script.js" defer></script>
+    <link rel="stylesheet" href="CSS/style.css">
+
+    <title>InfiniTea</title>
+</head>
+
+<body>
+    <h1>InfiniTea</h1>
+
+
+</body>
+
+</html>
