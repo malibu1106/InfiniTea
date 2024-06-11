5b6779a9d28ccf3082140a30fb2018a55cc71400
Mathilde Jourden <mathilde.jourden@gmail.com>
2024-06-11 11:14:20 +0200

    ajout de la chekbox

--- src/pages/edit_product.php
+++ src/pages/edit_product.php
@@ -27,6 +27,17 @@ echo '</pre>';
 ?>
 <form method="POST" action="pages/update_product.php" enctype="multipart/form-data"class="max-w-md mx-auto">
     <input type="hidden" name="added_by" value="<?php echo $_SESSION['user_id'];?>">
+    
+<div class="flex">
+    <div class="flex items-center h-5 my-5">
+        <input id="helper-checkbox" aria-describedby="helper-checkbox-text" type="checkbox" value="" class="w-4 h-4 text-purple-700  border-gray-300 rounded focus:ring-purple-800 ">
+    </div>
+    <div class="ms-2 text-sm">
+        <label for="helper-checkbox" class="font-medium text-gray-900 ">Modifier l'image</label>
+        <p id="helper-checkbox-text" class="text-xs font-normal text-gray-500 ">Souhaitez-vous modifier l'image du produit ?</p>
+    </div>
+</div>
+
 <div class="relative z-0 w-full mb-5 group">
       <input name="name" id="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-purple-700 peer" placeholder="" value="<?=$product['name']?>"required />
       <label for="produit" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nom du produit</label>
