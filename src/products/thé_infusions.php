642edf3eef76e134717fe53d2cb1491aba673635
Mathilde Jourden <mathilde.jourden@gmail.com>
2024-06-12 16:40:47 +0200

    tout les thés!

--- src/products/thé_infusions.php
+++ src/products/thé_infusions.php
@@ -22,3 +22,31 @@ else{
     echo "Aucune infusion";
 }
 ?>
+
+<div id="infusions2"><div class="flex flex-wrap gap-8 justify-center items-center">
+<div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+      <h3>Encelade</h3>
+      <img src="../images/feuilles/feuilles_infusions.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+      <p class="h-7 text-center mb-4">Notes de camomille et de menthe</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+  <h3>Ganymède</h3>
+  <img src="../images/feuilles/feuilles_infusions.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+  <p class="h-7 text-center mb-4">Touches de gingembre et de citronelle</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Dione</h3>
+    <img src="../images/feuilles/feuilles_infusions.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Arômes d'échinacée et de réglisse</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Mercure</h3>
+    <img src="../images/feuilles/feuilles_infusions.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Touches de menthe et de fenouil</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Sedna</h3>
+    <img src="../images/feuilles/feuilles_infusions.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Notes de mélisse et d'orties</p>
+  </div>
+</div></div>
--- src/products/thé_noir.php
+++ src/products/thé_noir.php
@@ -12,13 +12,41 @@ $thes_noir = $query->fetchAll(PDO::FETCH_ASSOC);
 // THE NOIR
 if($thes_noir){
     echo "Affichage thé noir";
-    // foreach ($thes_noir as $the_noir){
-    //     echo '<pre>';
+ foreach ($thes_noir as $the_noir){
+     echo '<pre>';
     //     print_r($the_noir);
-    //     echo '</pre>';
-    // }
+        echo '</pre>';
+     }
 }
 else{
     echo "Aucun thé noir";
 }
 ?>
+
+<div id="noir2"><div class="flex flex-wrap gap-8 justify-center items-center">
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Éclipse</h3>
+    <img src="../images/feuilles/feuilles_noir.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Notes de cacao et de vanille</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Voie Lactée</h3>
+    <img src="../images/feuilles/feuilles_noir.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Touches de noix de coco et d'amande</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Supernova</h3>
+    <img src="../images/feuilles/feuilles_noir.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Arômes canelle et de zestes d'orange</p>
+      </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Nébuleuse</h3>
+    <img src="../images/feuilles/feuilles_noir.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Touches de lavande et de bleuet</p>
+  </div>
+  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">
+    <h3>Galaxie</h3>
+    <img src="../images/feuilles/feuilles_noir.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">
+    <p class="h-7 text-center mb-4">Notes de jasmin et pêche blanche</p>
+  </div>
+</div></div>
