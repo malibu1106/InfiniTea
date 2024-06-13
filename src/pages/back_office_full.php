f91e2e7deaeca2888a2e863c2c3e2625b244afd8
Malibu <malibu1106@gmail.com>
2024-06-13 15:51:00 +0200

    modifs nav

--- src/pages/back_office_full.php
+++
@@ -1,6 +0,0 @@
-
-<a href="index.php?page=add_product"><li>Ajouter un produit</li></a>
-<a href="index.php?page=product_list"><li>Gérer les produits</li></a>
-<a href="index.php?page=user_control"><li>Contrôle des utilisateurs</li></a>
-
-
