c4010b39c468c350a52079f9f1161a00b42ba5c7
Mathilde Jourden <mathilde.jourden@gmail.com>
2024-06-17 10:30:04 +0200

    highlight, coffret, accessoires

--- src/pages/accessoires_grid.php
+++ src/pages/accessoires_grid.php
@@ -1,36 +1,5 @@
-<?php
-
-// CONNEXION A LA BDD
-require_once("elements/open_bdd.php");
-
-// REQUETE AVEC CE QU'IL NOUS FAUT A STOCKER EN $_SESSION SI BESOIN 
-$sql = "SELECT * FROM products WHERE highlight = TRUE";
-
-// PREPARATION DE LA REQUETE
-$query = $db->prepare($sql);
-
-//EXECUTION DE LA REQUETE + STOCK DES DONNEES DANS LA VARIABLE
-$query->execute();
-$highlights = $query->fetchAll(PDO::FETCH_ASSOC);
-
-//FERMETURE DE LA CONNEXION A DECOMMENTER APRES LES TESTS P-E
-//require_once("elements/close_bdd.php");
-
-// SI DES PRODUITS SONT HIGHLIGHT?>
-<section class="highlights"><h3>Nos produits à la une</h3>
-    <div id="left_arrow"><</div>
-    <div id="right_arrow">></div>
-    <?php foreach($highlights as $highlight){
-    echo '<div class="highlights_products">';
-    echo '<img src="' . $highlight['image_filename'] . '">';
-    echo '<div class="highlights_details">';
-    echo $highlight['name'];
-    echo '</div>';
-    echo '</div>';}
-?>
-</section>
-
-<section class="highlights_desktop"><h3>Nos produits à la une</h3>
+<div class="p-1 flex flex-col items-center justify-center">
+    <h3>Tout pour un tea time dans votre univers</h3>
 <div class="p-1 flex flex-wrap items-center justify-center">
 
     <div class="flex-shrink-0 m-6 relative overflow-hidden  rounded-lg max-w-xs shadow-lg">
@@ -41,7 +10,7 @@ $highlights = $query->fetchAll(PDO::FETCH_ASSOC);
         </svg>
         <div class="relative pt-10 px-10 flex items-center justify-center">
             
-            <img class="relative w-40" src="./images/produits/vert.png" alt="">
+            <img class="relative w-40" src="./images/selection/theiere_vert.png" alt="">
         </div>
         <div class="relative text-white px-6 pb-6 mt-6">
             <div class="flex justify-between">
@@ -58,7 +27,7 @@ $highlights = $query->fetchAll(PDO::FETCH_ASSOC);
         </svg>
         <div class="relative pt-10 px-10 flex items-center justify-center">
             
-            <img class="relative w-40" src="./images/produits/noir.png" alt="">
+            <img class="relative w-40" src="./images/selection/theiere_vert.png" alt="">
         </div>
         <div class="relative text-white px-6 pb-6 mt-6">
             <div class="flex justify-between">
@@ -74,7 +43,7 @@ $highlights = $query->fetchAll(PDO::FETCH_ASSOC);
         </svg>
         <div class="relative pt-10 px-10 flex items-center justify-center">
             
-            <img class="relative w-40" src="./images/produits/blanc.png" alt="">
+            <img class="relative w-40" src="./images/selection/theiere_vert.png" alt="">
         </div>
         <div class="relative text-white px-6 pb-6 mt-6">
             <div class="flex justify-between">
@@ -83,7 +52,21 @@ $highlights = $query->fetchAll(PDO::FETCH_ASSOC);
             </div>
         </div>
     </div> 
-
-    </section>
-
-
+    <div class="flex-shrink-0 m-6 relative overflow-hidden  rounded-lg max-w-xs shadow-lg">
+        <svg class="absolute bottom-0 left-0 mb-8" viewBox="0 0 375 283" fill="none"
+            style="transform: scale(1.5); opacity: 0.1;">
+            <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)" fill="white" />
+            <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="white" />
+        </svg>
+        <div class="relative pt-10 px-10 flex items-center justify-center">
+            
+            <img class="relative w-40" src="./images/selection/theiere_vert.png" alt="">
+        </div>
+        <div class="relative text-white px-6 pb-6 mt-6">
+            <div class="flex justify-between">
+                <span class="block font-semibold text-xl">Nom produit</span>
+                <span class="block rounded-full text-purple-500 text-xs font-bold px-3 py-2 leading-none flex items-center">X €</span>
+            </div>
+        </div>
+    </div>
+    </div></div>
