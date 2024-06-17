b67169a71fe8146e2bb20d4ec275c676cfb74bf4
Malibu <malibu1106@gmail.com>
2024-06-17 16:13:57 +0200

    nav updated

--- src/elements/nav.php
+++ src/elements/nav.php
@@ -2,17 +2,19 @@
     <button id="burgerButton">☰</button>
     <ul id="burger">
         <a href="../index.php"><li>Accueil</li></a>
-        <?php if(isset($_SESSION["user_id"])){
+        <?php 
+        if(empty($_SESSION['admin'])){
+            echo '<a href="../index.php#main"><li>Nos Produits</li></a>';
+            echo '<a href="../index.php?page=contact#main"><li>Contact</li></a>';
+            
+            }
+            if(isset($_SESSION["user_id"])){
             echo '<a href="../pages/logout.php"><li>Déconnexion</li></a>';
         }
         else{
             echo '<a href="../index.php?page=connexion#main"><li>Connexion</li></a>';
         }
-        if(empty($_SESSION['admin'])){
-        echo '<a href="../index.php#main"><li>Nos Produits</li></a>';
-        echo '<a href="../index.php?page=contact#main"><li>Contact</li></a>';
         
-        }
          if(!empty($_SESSION['admin'])){
             
 
