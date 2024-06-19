bc9567a47c2bd0d55c92ab5c0f48dd3921c5f693
m.jourden <74381521+mathildejrdn@users.noreply.github.com>
2024-06-19 11:43:18 +0200

    gestion des tailles de conteneur tailwind

--- src/products/thé_rooibos.php
+++ src/products/thé_rooibos.php
@@ -15,7 +15,7 @@ $thes_rooibos = $query->fetchAll(PDO::FETCH_ASSOC);
 <?php
     foreach ($thes_rooibos as $the_rooibos){
       echo '<a href="../index.php?page=product&cat=the&id=' . $the_rooibos['id'] . '#main">';
-      echo '<div class="hiddenProduct w-full sm:w-1/2 md:w-80 lg:w-80 xl:w-80 min-w-20  mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">';
+      echo '<div class="hiddenProduct w-full sm:w-full md:w-80 lg:w-80 xl:w-80 min-w-20  mb-4 flex flex-col items-center bg-stone-100 p-4 rounded-lg shadow-md">';
       echo '<h3 class="sm:text-5xl md:text-5xl lg:text-5xl xl:text-6xl mb-1">'.$the_rooibos['name'].'</h3>';
       echo '<img src="../images/feuilles/feuilles_rooibos.png" alt="feuilles de thé" class="w-1/4 h-1/4 mb-2">';
       echo '<p class="h-7 text-center mb-4">'.$the_rooibos['description_courte'].'</p>';
